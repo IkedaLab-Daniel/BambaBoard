19b06cc98563298dff1acc52e01fb354a9a9aedf
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-11-06 12:20:00 +0800

    Client: Adjust Grid Design

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,10 @@
 import './App.css'
-import Navigation from './components/navigation'
+import Hero from './components/Hero'
 
 function App() {
   return (
-    <div className='main text-slate-50 w-full h-screen flex flex-col items-center'>
-      <Navigation />
-      <h1 className='text-2xl'>Hello Hi</h1>
+    <div className='main'>
+      <Hero />
     </div>
   )
 }
---
+++ client/src/components/Hero.tsx
@@ -0,0 +1,19 @@
+
+const Hero: React.FC = () => {
+  return (
+    <div className='bg-slate-500'>
+        <div>
+            <h1>The Quiz Board</h1>
+            <p>Answer random question, or create random question...</p>
+        </div>
+
+        <div>
+            <span>Start</span>
+            <span>Create</span>
+        </div>
+       
+    </div>
+  )
+}
+
+export default Hero
