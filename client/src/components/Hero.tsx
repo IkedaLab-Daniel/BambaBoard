824d7175724770931107c59bf2be79d3044ce56f
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-11-07 00:20:48 +0800

    Client: Hero

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -1,15 +1,15 @@
 
 const Hero: React.FC = () => {
   return (
-    <div className='bg-slate-500'>
-        <div>
-            <h1>The Quiz Board</h1>
-            <p>Answer random question, or create random question...</p>
+    <div className='max-w-[1200px] mx-auto flex pt-20'>
+        <div className="w-[70%]">
+            <h1 className="text-6xl font-bold">The Quiz Board</h1>
+            <p className="text-xl">Answer random question, or create random question...</p>
         </div>
 
-        <div>
-            <span>Start</span>
-            <span>Create</span>
+        <div className="flex flex-col gap-2 w-[30%]">
+            <span className="border-3 border-[#435663] bg-[#435663] text-[#FFF8D4] font-bold p-4 text-center text-xl rounded-xl hover:cursor-pointer hover:scale-[105%] transition duration-500 active:scale-[97%] active:opacity-50">Start</span>
+            <span className="border-3 border-[#435663] text-[#435663] p-4 text-center text-xl font-bold rounded-xl hover:cursor-pointer hover:scale-[105%] transition duration-500 active:scale-[97%] active:opacity-50">Create</span>
         </div>
        
     </div>
